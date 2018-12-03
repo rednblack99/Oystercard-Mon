42834d296ae9c9a119c77c78eadea6fb239f6371
Adam Lodowski <adamlodowski@gmail.com>
2018-12-03 15:39:36 +0000

    minor amends

--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -10,12 +10,15 @@ RSpec.describe Oystercard do
   end
 
   it 'should raise an error if user tries to top up beyond maximum limit' do
-    expect{ subject.top_up(::MAX_BALANCE + 1) }.to raise_error {"Maximum balance exceeded"}
+    expect{ subject.top_up(::MAX_BALANCE + 1) }.to raise_error { "Maximum balance exceeded" }
   end
 
   it 'should deduct an amount from the card' do
     subject.top_up(20)
     expect(subject.deduct(5)).to eq 15
+  end
+
+  it 'should respond to in_journey' do
   end
 
 end
