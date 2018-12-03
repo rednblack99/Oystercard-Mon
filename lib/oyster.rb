61a13fe5a747eb6e9eb2ddbb13f7dfbda7237ead
Adam Lodowski <adamlodowski@gmail.com>
2018-12-03 15:26:24 +0000

    Oystercard now raises error when topping up beyond maximum limit and can deduct amounts from card

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,7 +1,19 @@
 class Oystercard
   attr_reader :balance
 
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
+
+def top_up(amount)
+  fail 'Max balance exceeded' if (amount + @balance) > MAX_BALANCE
+  @balance += amount
+end
+
+def deduct(amount)
+  @balance -= amount
+end
+
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Oystercard do
   it 'should return a balance of 0 when initialized' do
     expect(subject.balance).to eq 0
   end
+
+  it 'should top up the card by the amount given' do
+    expect(subject.top_up(5)).to eq 5
+  end
+
+  it 'should raise an error if user tries to top up beyond maximum limit' do
+    expect{ subject.top_up(::MAX_BALANCE + 1) }.to raise_error {"Maximum balance exceeded"}
+  end
+
+  it 'should deduct an amount from the card' do
+    subject.top_up(20)
+    expect(subject.deduct(5)).to eq 15
+  end
+
 end
